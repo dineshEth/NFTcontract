2579ffcf7482bdb5efcacd7badc84eede6d9fc29
Dinesh <dineshchauhan416pd@gmail.com>
2022-09-04 20:53:59 +0530

    done

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,18 +1,25 @@
+// import hardhat
 const hre  = require('hardhat');
 
 async function main (){
 
+//getting or reading the contract 
   const NFT = await hre.ethers.getContractFactory("NFT");
 
+  // deploing contract
   const nft = await NFT.deploy();
 
+  //awaits to deployed 
   await nft.deployed();
 
+  // address is very important so keep it 
   console.log("contract deployed to address : ",nft.address);
+
 
   console.log("successfully deployed :)");
 }
 
+// call to main function
 main()
 .then(()=>process.exit(0))
 .catch((error)=>{
